02e2b677655b493133b4c75c3e51e1e3e868f0df
Mariusz <poczta@mariuszb.eu>
2019-09-29 05:43:59 +0200

    add bundle Doctrine

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
@@ -12,15 +14,31 @@ class ToDoListController extends AbstractController
      */
     public function index()
     {
-        return $this->render('index.html.twig');
+    	$tasks = $this->getDoctrine()->getRepository(Task::class)->findAll();
+    	dump($tasks);
+
+        return $this->render('index.html.twig', compact('tasks'));
     }
 
     /**
      * @Route("/create", name="create_task", methods={"POST"})
      */
-    public function create()
+    public function create(Request $request)
     {
-        exit('to do: create a new task');
+    	$title = trim($request->get('title'));
+
+    	if (empty($title)) {
+    		return $this->redirectToRoute('to_do_list');
+	    }
+
+	    $entityManager = $this->getDoctrine()->getManager();
+
+	    $task = new Task();
+	    $task->setTitle($title);
+	    $entityManager->persist($task);
+	    $entityManager->flush();
+
+	    return $this->redirectToRoute('to_do_list');
     }
     
 	/**
